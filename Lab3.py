31e4fd9e12ed1892f5e1397d1ecc5858c4c10b61
Senesie Sannoh <Sannohs0506@Students.bowiestate.edu>
2023-10-16 14:14:43 -0400

    Lab 2files

---
+++ Lab3.py
@@ -0,0 +1,23 @@
+import math
+
+number1 = input('enter number 1')
+number2 = input('enter number 2')
+number3 = input('enter number 3')
+sum= int(number1) + int(number2) + int(number3)
+print(sum)
+difference = int(number1) - int(number2) - int(number3)
+multiplication = int(number1) * int(number2) * int(number3)
+division = int(number1) / int(number2) / int(number3)
+print(difference, multiplication, division)
+#Return the value of 9 raised to the power of 3
+print(math.pow(9, 3))
+print(math.acos(0.22))
+print(math.acosh(7))
+print(math.asin(0.43))
+print(math.asinh(3))
+print(math.atan(0.31))
+print(math.atan2(5, 8))
+print(math.atanh(0.78))
+print(math.ceil(1.9))
+print(math.cosh(8))
+print(math.fabs(-6))
